c92ae8171fcf5e3a06e5a38bc556ab3123b27a99
getwes <wesleyfreire1707@gmail.com>
2022-03-10 21:33:25 -0300

    primeiros codigos em java

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentloaded',() => {
     const tiles = array.from(document.querySelectorAll('.tile'));
-    
+    const playerdisplay = document.querySelector('.display-player');
+    const resetbutton = document.querySelector('#reset');
+    const announcer = document.querySelector('.annoucer');
 
     resetbutton.addEventListener('click', resetboard);
 
